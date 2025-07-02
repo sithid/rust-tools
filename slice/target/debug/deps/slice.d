71d10bdf96408b949f7de79563c54114029fc5a5
sithid <demonicurges05@gmail.com>
2025-07-02 01:32:27 -0400

    cleanup

--- slice/target/debug/deps/slice.d
+++
@@ -1,7 +0,0 @@
-D:\repos\rust-tools\slice\target\debug\deps\slice.d: src\main.rs
-
-D:\repos\rust-tools\slice\target\debug\deps\slice.exe: src\main.rs
-
-src\main.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.1.0
--- slice/target/debug/slice.d
+++
@@ -1,1 +0,0 @@
-D:\repos\rust-tools\slice\target\debug\slice.exe: D:\repos\rust-tools\slice\src\main.rs
